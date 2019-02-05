b51ad40be7e0ecc4ca5008712a7331fd800e1381
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-02-04 21:33:16 -0500

    template committ

---
+++ website/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.urls import path
+from .views import shifts_available_page_view
+from . import views
+# Application Routes (URLs)
+
+app_name = 'website'
+
+urlpatterns = [
+    	# General Page Views
+	#	url(r'^$', views.homepage_view, name='homepage_view'),]
+]
---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+import json
+import datetime
+from datetime import timedelta
+from django.utils import timezone
+
+#-------------------------------------------------------------------------------
+# Page Views
+#-------------------------------------------------------------------------------
